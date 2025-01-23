bd7da177195d40f08b385cb1478a6cba0ed241a8
XBlayz <stefano.scarcelli15@gmail.com>
2025-01-23 20:31:49 +0100

    Aggiornamento repo

--- script/debugOpenMP_64.sh
+++ script/debugOpenMP_64.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd ../src/openMP_x64/
-gcc -Wall -pg -m64 -msse -mavx -O0 -no-pie pst_OpenMP_64c.c -o pst_OpenMP_64_debug -lm -fopenmp
+for f in $(ls *64.nasm); do
+	nasm -f elf64 $f
+done
+gcc -Wall -pg -m64 -msse -mavx -O0 -no-pie *64.o pst_OpenMP_64c.c -o pst_OpenMP_64_debug -lm -fopenmp
